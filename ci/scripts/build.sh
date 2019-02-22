f96b17f3a795b1c0e4ec712f1d396dccd6b03ced
Ben Jackson <b@benjackson.email>
2019-02-22 22:06:27 +1100

    add start of ci pipeline

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -16,7 +16,7 @@ git checkout v0.3.0
 cd  /go/src/github.com/shreddedbacon/concourse-slackbot
 echo ">> Get"
 go get -v .
-cd -
+cd /go/src
 echo ">> Build"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o built-release/concoursebot github.com/shreddedbacon/concourse-slackbot
 

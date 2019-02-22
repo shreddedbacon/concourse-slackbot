b5e1701d1d4d416939059e2b36de21010585ec00
Ben Jackson <b@benjackson.email>
2019-02-22 22:08:01 +1100

    add start of ci pipeline

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -4,7 +4,8 @@ set -e -u -x
 
 echo ">> Install git"
 apk add --no-cache git
-
+pwd
+VERSION=$(cat ${VERSION_FROM})
 # set up directory stuff for golang
 echo ">> Setup Directories"
 mkdir -p /go/src/github.com/shreddedbacon/
@@ -16,12 +17,11 @@ git checkout v0.3.0
 cd  /go/src/github.com/shreddedbacon/concourse-slackbot
 echo ">> Get"
 go get -v .
-cd /go/src
+cd -
 echo ">> Build"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o built-release/concoursebot github.com/shreddedbacon/concourse-slackbot
 
 echo ">> Create artifact"
-VERSION=$(cat ${VERSION_FROM})
 cp -r /go/src/github.com/shreddedbacon/concourse-slackbot/templates built-release/
 cd built-release
 tar czf concoursebot-linux-$VERSION.tar.gz concoursebot

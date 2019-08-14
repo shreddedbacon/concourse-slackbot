6a0771dd2d55fb2bebfe33f4efeae1acff8e5ce6
Ben Jackson <b@benjackson.email>
2019-08-14 22:32:02 +1000

    use go mod

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -9,16 +9,16 @@ header() {
 
 MAINDIR=`pwd`
 VERSION=$(cat ${VERSION_FROM})
-export GO111MODULE=on 
+export GO111MODULE=on
 # set up directory stuff for golang
 header "Setup"
 mkdir -p /go/src/github.com/shreddedbacon/
 ln -s $PWD/concoursebot-release /go/src/github.com/shreddedbacon/concourse-slackbot
 header "Get deps"
-go get github.com/nlopes/slack
-#v0.3.0 required, newer version has some issues
-cd /go/src/github.com/nlopes/slack
-git checkout v0.3.0 > /dev/null 2>&1
+# go get github.com/nlopes/slack
+# #v0.3.0 required, newer version has some issues
+# cd /go/src/github.com/nlopes/slack
+# git checkout v0.3.0 > /dev/null 2>&1
 cd  /go/src/github.com/shreddedbacon/concourse-slackbot
 go get -v .
 cd $MAINDIR

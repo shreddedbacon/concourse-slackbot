e2c90468e028c1981523d07413b11fc77749a1f6
Ben Jackson <b@benjackson.email>
2019-08-14 22:49:36 +1000

    fixing build

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -28,3 +28,4 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o built-release/concoursebot 
 header "Create artifact"
 cd built-release
 tar czf concoursebot-linux-$VERSION.tar.gz concoursebot
+ls
